4c4dc9567ed836cd39bf8b2e259ed76042c8d045
Xavier Re4 <xrthefrench@gmail.com>
2019-02-25 10:37:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementsByClassName("coucou")[0].innerHTML = "salut"
+
+//* fonctionalite 1
+function (footy) {
+
+document.getElementById("footer").addEventListener("click")
+console.log("clique")
+}
+footy()
