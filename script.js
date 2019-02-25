99a2c5a8e4478a8346b0c56b1bd9c786f982ac70
quentinvdcdc <quentin.vandecandelaere@gmail.com>
2019-02-25 15:30:57 +0100

    YOLO2

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 let footer = document.querySelector("footer");
 let i = 0;
+let viewToggle = [];
 let navbarHeader = document.querySelector('#navbarHeader');
 let navbarToggler = document.querySelectorAll(".navbar-toggler")[0];
+let viewButtonIndexZero = document.querySelectorAll(".btn-success")[0];
 let editButtonIndexZero = document.querySelectorAll(".btn-outline-secondary")[0];
 let textCardIndexZero = document.querySelectorAll(".card-text")[0];
+let imgCardIndexZero = document.querySelectorAll(".card-img-top")[0];
 let editButtonIndexOne = document.querySelectorAll(".btn-outline-secondary")[1];
 let textCardIndexOne = document.querySelectorAll(".card-text")[1];
+let allCards = document.querySelectorAll('.col-md-4');
 let greenToggler = false;
 console.log(document.querySelector('.navbar'));
 let navbar = document.querySelector('.navbar');
 let linkBootstrap = document.querySelector('link[rel=stylesheet]');
-let allCards = document.querySelectorAll(".btn-outline-secondary");
+console.log(document.querySelectorAll('.col-md-4'));
+console.log(document.querySelector('.navbar-brand'));
+let navbarBrand = document.querySelector('.navbar-brand');
 
 
 footer.addEventListener('click', function(){
@@ -48,4 +54,45 @@ navbar.addEventListener('dblclick', function(){
 	else {
 		linkBootstrap.disabled = false;
 	};
+});
+
+for (let k = 0; k < allCards.length; k++){
+	viewToggle.push(false);
+	document.querySelectorAll(".btn-success")[k].addEventListener('mouseover', function(){
+		if (viewToggle[k] === false){
+			document.querySelectorAll(".card-text")[k].style.display = "none";
+			document.querySelectorAll(".card-img-top")[k].style.width = '20%';
+			viewToggle[k] = true;
+		}
+		else {
+			document.querySelectorAll(".card-text")[k].style.display = "block";
+			document.querySelectorAll(".card-img-top")[k].style.width = '100%';
+			viewToggle[k] = false;
+		};
+	});
+}
+
+navbarBrand.addEventListener('keypress', (event) => {
+  switch (event.key) {
+  case "a":
+  	document.querySelector('body').classList.remove('offset-md-4');
+  	document.querySelector('body').classList.remove('offset-md-8');
+    document.querySelector('body').classList.add('col-4');
+    break;
+  case "y":
+  	document.querySelector('body').classList.remove('offset-md-8');
+    document.querySelector('body').classList.add('col-4');
+    document.querySelector('body').classList.add('offset-md-4');
+    break;
+  case "p":
+  	document.querySelector('body').classList.remove('offset-md-4');
+    document.querySelector('body').classList.add('col-4');
+    document.querySelector('body').classList.add('offset-md-8');
+    break;
+  case "b":
+    document.querySelector('body').classList.remove('col-4');
+    document.querySelector('body').classList.remove('offset-md-4');
+    document.querySelector('body').classList.remove('offset-md-8');
+    break;
+  };
 });
